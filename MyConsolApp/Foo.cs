f800a30c4e26019b3e2436c10f7bd171c7428315
Andres Corallo <acorallo@gmail.com>
2015-07-14 21:34:43 -0300

    Primer commit arrancando el proyecto

---
+++ MyConsolApp/Foo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsolApp
+{
+    public class Foo
+    {
+        private int dummyCont = 0;
+
+
+        public int GetDummyCont()
+        {
+            return this.dummyCont;
+        }
+
+        public void SetDummyCont(int valor)
+        {
+            this.dummyCont = valor;
+        }
+
+    }
+}
---
+++ MyConsolApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta es una prueba");
+            Console.WriteLine("Pausa");
+
+            Console.ReadLine();
+        }
+    }
+}
