797bb98894ed135c6eb1438dac5898223ce6ff91
Andres Corallo <acorallo@gmail.com>
2015-07-15 00:40:26 -0300

    Segunda version

---
+++ MyConsolApp/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsolApp
+{
+    class MyClass
+    {
+        // Esta es una clase que se agrego despues.
+    }
+}
+
